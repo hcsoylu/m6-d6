2725b76857cff087abb9b350f815dbf33d44acd4
Can Soylu <hsyncn17@gmail.com>
2021-05-03 17:49:04 +0300

    created table

---
+++ student.sql
@@ -0,0 +1,158 @@
+CREATE TABLE IF NOT EXISTS
+    	students(
+        	student_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name VARCHAR(50) NOT NULL,
+          	last_name VARCHAR(50) NOT NULL,
+            country VARCHAR(50) NOT NULL
+        );
+        
+   INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Gentrit',
+      'Begaj',
+      'Turkey'
+    );
+    
+    INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Slavko',
+      'Josipovic',
+      'Turkey'
+    );
+    
+    INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Luca',
+      'Perullo',
+      'Turkey'
+    );
+    
+    
+    INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Ardi',
+      'Germenji',
+      'Turkey'
+    );
+    
+    
+     INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Jamie',
+      'Ellis',
+      'Turkey'
+    );
+    
+     INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Hüseyin Can',
+      'Soylu',
+      'Turkey'
+    );
+    
+    
+     INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Hafiz',
+      'Ali',
+      'Turkey'
+    );
+     
+    
+    
+        INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Juan',
+      'Arana',
+      'Turkey'
+    );
+    
+        INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Mihai',
+      'Ivanov',
+      'Turkey'
+    );
+       
+      
+          INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'David',
+      'Zapata',
+      'Turkey'
+    );
+    
+        INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Paul',
+      'Balu',
+      'Turkey'
+    );
+    
+    
+         INSERT INTO
+	students(
+      name,
+      last_name,
+      country
+    )
+    VALUES (
+      'Sean',
+      'Knowles',
+      'Turkey'
+    );
---
+++ tutors.sql
@@ -0,0 +1,115 @@
+CREATE TABLE IF NOT EXISTS
+    tutors(
+        tutor_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      	name VARCHAR(50) NOT NULL,
+      	last_name VARCHAR(50) NOT NULL,
+      	country VARCHAR (50) NOT NULL
+        );
+
+INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Diego',
+    'Banovaz',
+    'Italy'
+  );
+  
+INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Riccardo',
+    'Gulin',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Stefano',
+    'Cassasola',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Stefano',
+    'Micelli',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Luis',
+    'Antonio',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Tetiana',
+    'Yaremko',
+    'Ukraine'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Daniele',
+    'Banovaz',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Alessia',
+    'Luminari',
+    'Italy'
+  );
+  
+ INSERT INTO
+  tutors (
+		name,
+  		last_name,
+    	country
+	)
+  VALUES(
+  	'Ubeyt',
+    'Demir',
+    'Turkey'
+  );
